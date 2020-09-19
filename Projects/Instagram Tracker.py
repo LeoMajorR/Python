528b1cdbb56f348b57fbc0c4d06bdba2b5d019cf
leomajorr <ssingh.raviprakash@gmail.com>
2020-09-19 15:27:37 +0530

    Adding Instagram Tracker

---
+++ Projects/Instagram Tracker.py
@@ -0,0 +1,71 @@
+"""
+Author : Ravi Prakash Singh
+Date   : September 19th, 2020
+"""
+
+# importing libraries
+from bs4 import BeautifulSoup
+import requests
+
+# instagram URL
+URL = "https://www.instagram.com/{}/"
+
+
+# parse function
+def parse_data(s):
+
+    # creating a dictionary
+    data = {}
+
+    # splittting the content
+    # then taking the first part
+    s = s.split("-")[0]
+
+    # again splitting the content
+    s = s.split(" ")
+
+    # assigning the values
+    data['Followers'] = s[0]
+    data['Following'] = s[2]
+    data['Posts'] = s[4]
+
+    # returning the dictionary
+    return data
+
+
+# scrape function
+def scrape_data(username):
+
+    # getting the request from url
+    r = requests.get(URL.format(username))
+
+    # converting the text
+    s = BeautifulSoup(r.text, "html.parser")
+
+    # finding meta info
+    meta = s.find("meta", property="og:description")
+
+    # calling parse method
+    return parse_data(meta.attrs['content'])
+
+
+# main function
+if __name__ == "__main__":
+
+    # user name
+    username = input("Enter username you want to track:> ")
+
+    # calling scrape function
+    data = scrape_data(username)
+
+    # printing the info
+    print()
+    print("+------------+------------+--------+")
+    print("| {:<10} | {:<10} | {:<6} |".format('Followers', 'Following',
+                                               'Posts'))
+    print("+------------+------------+--------+")
+    print("| {:<10} | {:<10} | {:<6} |".format(data["Followers"],
+                                               data["Following"],
+                                               data["Posts"]))
+    print("+------------+------------+--------+")
+    print()
